1e72c5855bd3f7e5cacbbf87281542ecf6780741
Artem <artemzhdanov1299@gmail.com>
2023-10-04 14:14:45 +0300

    только боковые кнопки сделал

--- button_work/MainWindow.xaml.cs
+++ button_work/MainWindow.xaml.cs
@@ -33,5 +33,20 @@ namespace button_work
         {
 
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
